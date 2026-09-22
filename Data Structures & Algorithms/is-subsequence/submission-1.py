9f3734de499eaf2613e366e0964ca54b133b0b41
Sandip Giri <160814463+SandipGiri-Developer@users.noreply.github.com>
2026-09-23 00:42:01 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        first = second = 0
+        while first<len(s) and second<len(t):
+            if s[first]==t[second]:
+                first+=1
+                second+=1
+            else:
+                second+=1
+        return first==len(s)
